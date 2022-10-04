57414c783a72f2e9fc9ae766337eac00081b55e4
taehyunL <taylor1233@naver.com>
2022-10-04 12:10:48 +0900

    initial commit

---
+++ 1st_Seminar/1st_Assignment_Basic/1st_Assignment_Basic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1st_Assignment_Basic
+//
+//  Created by 이태현 on 2022/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
