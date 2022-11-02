3c25ad20caa4313a74866efe8f6a3eaac19a336a
taehyunL <taylor1233@naver.com>
2022-11-02 23:51:45 +0900

    [FEAT] 친구 목록에 사용될 model

---
+++ 1st_Seminar/1st_Assignment_Basic/1st_Assignment_Basic/FriendListScene/Model/FriendListModel.swift
@@ -0,0 +1,16 @@
+//
+//  FriendListModel.swift
+//  1st_Assignment_Basic
+//
+//  Created by 이태현 on 2022/10/31.
+//
+
+import Foundation
+
+struct FriendListModel {
+    var image: String
+    var name: String
+    var explain: String
+}
+
+extension FriendListModel: Hashable {}
