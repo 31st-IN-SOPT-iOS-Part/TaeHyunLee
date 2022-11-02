24a9c1bb19ee09b20a9236ef7c56e3798bf2e5ee
taehyunL <taylor1233@naver.com>
2022-11-02 23:54:04 +0900

    [FEAT] tabbar를 위한 VC 설정

---
+++ 1st_Seminar/1st_Assignment_Basic/1st_Assignment_Basic/SharpScene/VC/SharpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SharpViewController.swift
+//  1st_Assignment_Basic
+//
+//  Created by 이태현 on 2022/10/31.
+//
+
+import UIKit
+
+class SharpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
