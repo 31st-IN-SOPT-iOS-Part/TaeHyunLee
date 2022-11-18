2ba67b7c4d3976701569a6f361361bad7b79893e
taehyunL <taylor1233@naver.com>
2022-11-18 15:05:55 +0900

    [ADD] 필요한 section 추가

--- 1st_Seminar/1st_Assignment_Basic/1st_Assignment_Basic/FriendListScene/Model/FriendListModel.swift
+++ 1st_Seminar/1st_Assignment_Basic/1st_Assignment_Basic/FriendListScene/Model/FriendListModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import RxDataSources
 
 struct FriendListModel {
     var image: String
@@ -14,3 +15,31 @@ struct FriendListModel {
 }
 
 extension FriendListModel: Hashable {}
+
+extension FriendListModel: IdentifiableType, Equatable {
+    var identity: String {
+        return UUID().uuidString
+    }
+    
+}
+
+struct FriendSection {
+    var header: String
+    var items: [FriendListModel]
+}
+
+extension FriendSection: AnimatableSectionModelType {
+    
+    typealias Item = FriendListModel
+    
+    var identity: String {
+        return header
+    }
+    
+    init(original: FriendSection, items: [FriendListModel]) {
+        self = original
+        self.items = items
+    }
+    
+}
+
